d82ede440c565b5466c3382a76b5a638a64c26e0
Ayumu Saito <saito.ayumu1002@icoud.com>
2026-01-03 23:52:05 +0900

    Add a command

--- build/twist_stdin_bridge/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/saito/ros2_ws/src/twist_stdin_bridge/install/twist_stdin_bridge'
--- build/twist_stdin_bridge/share/twist_stdin_bridge/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/saito/ros2_ws/src/twist_stdin_bridge/build/twist_stdin_bridge"
